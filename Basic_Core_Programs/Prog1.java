64570a22775870155082a7697146c57816803b45
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-20 17:14:10 +0530

    First Program Commit

---
+++ Basic_Core_Programs/Prog1.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class Prog1{
+    public static void main(String[] args)
+    {
+      Scanner scanObj = new Scanner (System.in);
+      System.out.println("Enter the number of flips");
+      int flips=scanObj.nextInt();
+      int temp=flips;
+      int head=0,tail=0;
+      while(temp>0)
+        {
+          double hOrT=Math.random();
+          if(hOrT>0.5)
+           tail++;
+          else
+           head++;
+          temp--;
+        }
+      System.out.println("Percentage of Head is " + head*1.0/flips);
+      System.out.println("Percentage of Tail is " + tail*1.0/flips);
+    }
+}
